830077c5e12cfcb9082504de3890da30a2200cc1
Florient Ratel <f.ratel@webetsolutions.com>
2018-01-08 14:51:35 +0100

    Make validators on inputs

--- src/app/hours-input/hours-input.component.ts
+++ src/app/hours-input/hours-input.component.ts
@@ -1,59 +1,63 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import {Component, OnInit} from '@angular/core';
+import {FormGroup, FormControl, Validators} from '@angular/forms';
 
 @Component({
-  selector: 'app-hours-input',
-  templateUrl: './hours-input.component.html',
-  styleUrls: ['./hours-input.component.css']
+    selector: 'app-hours-input',
+    templateUrl: './hours-input.component.html',
+    styleUrls: ['./hours-input.component.css']
 })
 export class HoursInputComponent implements OnInit {
 
-  constructor() { }
-  form;
-
-  ngOnInit() {
-    this.form = new FormGroup({
-        day: new FormControl(Date.now()),
-        begin: new FormControl('', this.beginHoursValidator),
-        end: new FormControl('', this.endHoursValidator)
-    });
-  }
-
-  beginHoursValidator(control) {
-    let value;
-    value = control.value;
-    console.log(value);
-    if (value.match('[0-9]{2}h[0-9]{2}')) {
-        let hours;
-        hours = parseInt(value.substr(0, 2), 0);
-        let minutes;
-        minutes = parseInt(value.substr(3, 2), 0);
-        if (hours >= 0 && hours < 24 && minutes >= 0 && minutes < 60) {
-            return {'begin': true};
-        }
+    constructor() {
     }
 
-      return {'begin': false};
-  }
+    form;
 
-    endHoursValidator(control) {
-        let value;
-        value = control.value;
-        console.log(value);
-        if (value.match('[0-9]{2}h[0-9]{2}')) {
-            let hours;
-            hours = parseInt(value.substr(0, 2), 0);
-            let minutes;
-            minutes = parseInt(value.substr(3, 2), 0);
+    ngOnInit() {
+        this.form = new FormGroup({
+            day: new FormControl('', Validators.required),
+            begin: new FormControl('', this.beginHoursValidator),
+            end: new FormControl('', this.endHoursValidator)
+        });
+    }
+
+    beginHoursValidator(control: FormControl) {
+        const value = control.value;
+        if (value.match('^[0-9]{2}h[0-9]{2}$')) {
+            const hours = parseInt(value.substr(0, 2), 0);
+            const minutes = parseInt(value.substr(3, 2), 0);
             if (hours >= 0 && hours < 24 && minutes >= 0 && minutes < 60) {
-                return {'begin': true};
+                return null;
             }
         }
 
         return {'begin': false};
     }
 
-  addHours() {
-      console.log('added');
-  }
+    endHoursValidator(control: FormControl) {
+        const value = control.value;
+        if (value.match('^[0-9]{2}h[0-9]{2}$')) {
+            const hours = parseInt(value.substr(0, 2), 0);
+            const minutes = parseInt(value.substr(3, 2), 0);
+            const totalMinutes = (hours * 60) + minutes;
+
+            const beginValue = control.parent.get('begin').value;
+            const beginHours = parseInt(beginValue.substr(0, 2), 0);
+            const beginMinutes = parseInt(beginValue.substr(3, 2), 0);
+            const totalBeginMinutes = (beginHours * 60) + beginMinutes;
+
+            if (
+                (hours >= 0 && hours < 24 && minutes >= 0 && minutes < 60)
+                && (totalBeginMinutes < totalMinutes)
+            ) {
+                return null;
+            }
+        }
+
+        return {'end': false};
+    }
+
+    addSession = function(session) {
+        console.log(session);
+    }
 }
