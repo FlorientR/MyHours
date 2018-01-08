5dcda9dbf3169605e01928cf20b17e82b62dac1e
Florient Ratel <f.ratel@webetsolutions.com>
2018-01-08 09:41:28 +0100

    Test validator

--- src/app/hours-input/hours-input.component.ts
+++ src/app/hours-input/hours-input.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl} from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-hours-input',
@@ -14,9 +14,46 @@ export class HoursInputComponent implements OnInit {
   ngOnInit() {
     this.form = new FormGroup({
         day: new FormControl(Date.now()),
-        begin: new FormControl(),
-        end: new FormControl()
+        begin: new FormControl('', this.beginHoursValidator),
+        end: new FormControl('', this.endHoursValidator)
     });
   }
 
+  beginHoursValidator(control) {
+    let value;
+    value = control.value;
+    console.log(value);
+    if (value.match('[0-9]{2}h[0-9]{2}')) {
+        let hours;
+        hours = parseInt(value.substr(0, 2), 0);
+        let minutes;
+        minutes = parseInt(value.substr(3, 2), 0);
+        if (hours >= 0 && hours < 24 && minutes >= 0 && minutes < 60) {
+            return {'begin': true};
+        }
+    }
+
+      return {'begin': false};
+  }
+
+    endHoursValidator(control) {
+        let value;
+        value = control.value;
+        console.log(value);
+        if (value.match('[0-9]{2}h[0-9]{2}')) {
+            let hours;
+            hours = parseInt(value.substr(0, 2), 0);
+            let minutes;
+            minutes = parseInt(value.substr(3, 2), 0);
+            if (hours >= 0 && hours < 24 && minutes >= 0 && minutes < 60) {
+                return {'begin': true};
+            }
+        }
+
+        return {'begin': false};
+    }
+
+  addHours() {
+      console.log('added');
+  }
 }
