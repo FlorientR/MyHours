6ff7295fc8772b662c5c075cd28566f51abc64e0
Florient Ratel <f.ratel@webetsolutions.com>
2018-01-10 13:35:15 +0100

    Fix

--- src/app/format-minutes.pipe.ts
+++ src/app/format-minutes.pipe.ts
@@ -9,7 +9,7 @@ export class FormatMinutesPipe implements PipeTransform {
       let minutes = value % 60;
       let hours = (value - minutes) / 60;
 
-      hours = parseInt(hours);
+      hours = parseInt('' + hours);
 
       return (hours < 10 ? '0' + hours : hours) + 'h'
           + (minutes < 10 ? '0' + minutes : minutes);
